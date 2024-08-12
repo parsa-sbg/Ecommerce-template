f3eeae6b983bb104f052aa7eea8058e483f74331
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-12 11:40:18 +0330

    feat: coding categoties section

---
+++ src/components/Home/CategoryItem.jsx
@@ -0,0 +1,17 @@
+import propTypes from 'prop-types'
+
+export default function CategoryItem({Svg, name}) {
+    console.log(Svg);
+    
+  return (
+    <div className='hover:bg-red-600 hover:text-white transition-colors cursor-pointer rounded-md flex flex-col items-center justify-center gap-3 w-full h-full border-stone-300 border-2 font-medium p-10'>
+        <Svg size={50} />
+        {name}
+    </div>
+  )
+}
+
+CategoryItem.propTypes = {
+    Svg: propTypes.func,
+    name: propTypes.string
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,20 @@ import SectionHeader from "../components/Home/SectionHeader"
 import FlashSalesSlider from "../components/Home/FlashSalesSlider"
 import SiteBtn from "../components/SiteBtn"
 
+import CategoryItem from "../components/Home/CategoryItem"
+import { CiMobile4 } from "react-icons/ci";
+import { HiOutlineComputerDesktop } from "react-icons/hi2";
+import { BsSmartwatch } from "react-icons/bs";
+import { CiCamera } from "react-icons/ci";
+import { CiHeadphones } from "react-icons/ci";
+import { LuGamepad } from "react-icons/lu";
+
+
+
+
+
+
+
 
 export default function Home() {
   return (
@@ -32,6 +46,18 @@ export default function Home() {
         </div>
       </div>
 
+      {/* Categories */}
+      <div>
+        <SectionHeader flagName={'categories'} title={'Browse By Category'} />
+        <div className="flex justify-between">
+          <div className="w-48 h-40" ><CategoryItem Svg={CiMobile4} name={'Phones'} /></div>
+          <div className="w-48 h-40" ><CategoryItem Svg={HiOutlineComputerDesktop} name={'Computers'} /></div>
+          <div className="w-48 h-40" ><CategoryItem Svg={BsSmartwatch} name={'SmartWatch'} /></div>
+          <div className="w-48 h-40" ><CategoryItem Svg={CiCamera} name={'Camera'} /></div>
+          <div className="w-48 h-40" ><CategoryItem Svg={CiHeadphones} name={'HeadPhones'} /></div>
+          <div className="w-48 h-40" ><CategoryItem Svg={LuGamepad} name={'Gaming'} /></div>
+        </div>
+      </div>
 
     </div>
   )
