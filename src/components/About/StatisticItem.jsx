7a8f9a29deb9c583da8a9be1aa25c0fe2272e0ce
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-12 22:55:38 +0330

    feat: coding statistics section

---
+++ src/components/About/StatisticItem.jsx
@@ -0,0 +1,19 @@
+import propTypes from 'prop-types'
+import { RiNumber3 } from 'react-icons/ri'
+
+
+export default function StatisticItem({ Icon, number, desc }) {
+    return (
+        <div className="w-full h-full flex flex-col items-center justify-center gap-1 border-2 border-stone-400 p-7 hover:bg-red-600 hover:text-white transition-colors group rounded-md">
+            {<Icon className='bg-black mb-5 text-white rounded-full p-2 outline outline-8 outline-stone-400 group-hover:text-black group-hover:bg-white group-hover:outline-red-200 transition-colors' size={50} />}
+            <span className='font-bold text-2xl'>{number}k</span>
+            <span className='text-sm' >{desc}</span>
+        </div>
+    )
+}
+
+StatisticItem.propTypes = {
+    Icon: propTypes.func,
+    number: propTypes.number,
+    desc: propTypes.string
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,41 @@
+import StatisticItem from "../components/About/StatisticItem"
+import { CiShop } from "react-icons/ci";
+import { PiCurrencyCircleDollarLight } from "react-icons/pi";
+import { IoGiftSharp } from "react-icons/io5";
+import { FaSackDollar } from "react-icons/fa6";
+
+
+
+
+
 
 export default function About() {
   return (
-    <div className="mt-20">
+    <div className="">
 
 
-        {/* our story section */}
-        <div className="grid grid-cols-2 gap-5 ">
-            <div className="flex flex-col justify-center col-span-2 sm:col-span-1 order-1">
-                <h1 className="font-bold text-4xl">Our Story</h1>
-                <p className="mt-5 text-lg">Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. </p>
-                <p className="mt-5 text-lg">Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging  from consumer.</p>
-            </div>
-            <div className="col-span-2 sm:col-span-1 sm:order-3">
-                <img className="w-full h-full " src="/src/assets/images/ourstoryImages/ourstory.png" alt="" />
-            </div>
+      {/* our story section */}
+      <div className="grid grid-cols-2 gap-5 mt-10">
+        <div className="flex flex-col justify-center col-span-2 sm:col-span-1 order-1">
+          <h1 className="font-bold text-4xl">Our Story</h1>
+          <p className="mt-5 text-lg">Launced in 2015, Exclusive is South Asia’s premier online shopping makterplace with an active presense in Bangladesh. Supported by wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sallers and 300 brands and serves 3 millioons customers across the region. </p>
+          <p className="mt-5 text-lg">Exclusive has more than 1 Million products to offer, growing at a very fast. Exclusive offers a diverse assotment in categories ranging  from consumer.</p>
         </div>
+        <div className="col-span-2 sm:col-span-1 sm:order-3">
+          <img className="w-full h-full object-contain" src="/src/assets/images/ourstoryImages/ourstory.png" alt="" />
+        </div>
+      </div>
+
+
+      {/* statistics */}
+      <div className="grid grid-cols-12 mt-20 gap-5">
+        <div className="col-span-6 md:col-span-4 lg:col-span-3" ><StatisticItem number={10.5} desc={'Sallers active our site'} Icon={CiShop} /></div>
+        <div className="col-span-6 md:col-span-4 lg:col-span-3" ><StatisticItem number={33} desc={'Mopnthly Produduct Sale'} Icon={PiCurrencyCircleDollarLight} /></div>
+        <div className="col-span-6 md:col-span-4 lg:col-span-3" ><StatisticItem number={45.5} desc={'Customer active in our site'} Icon={IoGiftSharp} /></div>
+        <div className="col-span-6 md:col-span-4 lg:col-span-3" ><StatisticItem number={25} desc={'Anual gross sale in our site'} Icon={FaSackDollar} /></div>
+
+      </div>
+
 
     </div>
   )
