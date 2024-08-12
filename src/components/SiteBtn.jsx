8e4cc851509482216dfadfc6707cae78e13970ca
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-12 12:50:03 +0330

    fix : add text-nowrap to site-btn

--- src/components/SiteBtn.jsx
+++ src/components/SiteBtn.jsx
@@ -2,7 +2,7 @@ import propTypes from 'prop-types'
 
 export default function SiteBtn({text, noMargin}) {
   return (
-    <button className={`w-full h-full transition-colors px-6 rounded-md hover:bg-red-700 bg-red-600 text-white ${!noMargin && 'my-10'} py-3`}>{text}</button>
+    <button className={`w-full h-full transition-colors px-6 rounded-md hover:bg-red-700 bg-red-600 text-white text-nowrap ${!noMargin && 'my-10'} py-3`}>{text}</button>
   )
 }
 
