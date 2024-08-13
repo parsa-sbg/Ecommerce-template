e8cfc3b850de30fdb7b66e837eb0adb990ffbb54
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-13 22:30:36 +0330

    feat: save theme into localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import './App.css'
 import Header from './components/Header/Header'
 import Routes from './routes'
 import Footer from './components/Footer'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ToggleThemeBtn from './components/ToggleThemeBtn'
 
 
 function App() {
 
   const [isDark, setIsDark] = useState(true)
+
+  useEffect(() => {
+    const theme = JSON.parse(localStorage.getItem('theme'))
+    setIsDark(theme)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('theme', isDark)
+  }, [isDark])
+
+
 
   return (
     <div className={`app ${isDark && 'dark'} transition-colors text-black dark:text-white bg-white dark:bg-inherit`}>
